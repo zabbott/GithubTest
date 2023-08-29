668a2a05fb399dc8ec83072a8659fc6ca218e008
zabbott <zabbott904@gmail.com>
2023-08-29 09:50:39 -0500

    Update ScriptMadeOnZachBranch.cs

--- Assets/Scenes/ScriptMadeOnZachBranch.cs
+++ Assets/Scenes/ScriptMadeOnZachBranch.cs
@@ -8,7 +8,7 @@ public class ScriptMadeOnZachBranch : MonoBehaviour
     public string Message; 
     void Start()
     {
-        print(Message);
+        print("Garbage hard coded text");
     }
 
     // Update is called once per frame
