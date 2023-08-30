993341d4ae30f636b734a12a7418b34eb912abc4
TheLastAce <59660291+TheLastAce@users.noreply.github.com>
2023-08-30 09:46:07 -0500

    Update ScriptMadeOnZachBranch.cs

--- Assets/Scenes/ScriptMadeOnZachBranch.cs
+++ Assets/Scenes/ScriptMadeOnZachBranch.cs
@@ -9,7 +9,7 @@ public class ScriptMadeOnZachBranch : MonoBehaviour
     void Start()
     {
         Debug.Log("Test debug");
-        print("Some excelent text. this text is so great!" + Message);
+        print("I like to draw cats =^w^=" + Message);
         print("Hey this is even better than charlies text");
     }
 
